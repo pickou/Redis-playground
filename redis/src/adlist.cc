792dff156368d93eacbab08bd3004826144b2124
junchaoyan <junchaoyan@tencent.com>
2020-03-23 09:18:43 +0800

    add list

---
+++ redis/src/adlist.cc
@@ -0,0 +1,208 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "redis/src/adlist.h"
+#include "redis/src/zmalloc.h"
+
+listNode *listFirst(list *list) {
+    return list->head;
+}
+
+listNode *listLast(list *list) {
+    return list->tail;
+}
+
+size_t listLength(list *list) {
+    return list->len;
+}
+
+listNode *listPrevNode(listNode *node) {
+    return node->prev;
+}
+
+listNode *listNextNode(listNode *node) {
+    return node->next;
+}
+
+void *listNodeValue(listNode *node) {
+    return node->value;
+}
+
+list *listCreate(void) {
+    struct list *list = NULL;
+    list = (struct list *)zmalloc(sizeof(struct list));
+    if(list == NULL) {
+        return NULL;
+    }
+    list->head = NULL;
+    list->tail = NULL;
+    list->len = 0;
+    list->dup = NULL;
+    list->free = NULL;
+    list->match = NULL;
+    return list;
+}
+
+void listRelease(list *list) {
+    if(list == NULL) return;
+    // free each node
+    listNode *curr, *next;
+    size_t len = list->len;
+    curr = next = list->head;
+    while(len--) {
+        next = curr->next;
+        if(list->free) list->free(curr->value);
+        zfree(curr);
+        curr = next;
+    }
+    // free list
+    safe_zfree(&list);
+}
+
+void safe_listRelease(list **list) {
+    if(*list == NULL) return;
+    listNode *curr, *next;
+    size_t len = (*(list))->len;
+    curr = next = (*(list))->head;
+    while(len--) {
+        next = curr->next;
+        if((*(list))->free) (*(list))->free(curr->value);
+        safe_zfree(&curr);
+        curr = next;
+    }
+    safe_zfree(list);
+}
+
+list *listAddNodeHead(list *list, void *value) {
+    listNode *node = (listNode *)zmalloc(sizeof(listNode*));
+    node->prev = NULL;
+    node->next = list->head;
+    (list->head)->prev = node;
+    node->value = value;
+    list->head = node;
+    list->len += 1;
+    return list;
+}
+
+list *listAddNodeTail(list *list, void *value) {
+    listNode *node = (listNode *)zmalloc(sizeof(listNode*));
+    node->prev = list->tail;
+    node->next = NULL;
+    (list->head)->next = node;
+    node->value = value;
+    list->len += 1;
+    return list;
+}
+
+void listDelNode(list *list, listNode *node) {
+    if(node == list->head) {
+        list->head = node->next;
+        (node->next)->prev = NULL;
+    } else if(node == list->tail) {
+        list->tail = node->prev;
+        (node->prev)->next = NULL;
+    } else {
+        (node->prev)->next = node->next;
+        (node->next)->prev = node->prev;
+    }
+    if(list->free) list->free(node->value);
+    zfree(node);
+    list->len--;
+}
+
+listNode *listSearchKey(list *list, void *key) {
+    listIter *iter;
+    listNode *node;
+
+    iter = listGetIterator(list, FORWARD);
+    while((node = listNext(iter)) != NULL) {
+        if(list->match) {
+            if(list->match(node->value, key)) {
+                listReleaseIter(iter);
+                return node;
+            }
+        } else {
+            if(key == node->value) {
+                listReleaseIter(iter);
+                return node;
+            }
+        }
+    }
+    listReleaseIter(iter);
+    return NULL;
+}
+
+listNode *listIndex(list *list, int index) {
+    int i = 0;
+    listNode *node = list->head;
+    while(node) {
+        if(i == index) return node;
+        node = node->next;
+    }
+    return NULL;
+}
+
+list *listDup(list *orig) {
+    list *copy = listCreate();
+    copy->dup = orig->dup;
+    copy->free = orig->free;
+    copy->match = orig->match;
+
+    struct listIter *iter = listGetIterator(orig, FORWARD);
+    while(iter->next) {
+        listNode *node = listNext(iter);
+        void *value;
+        if(copy->dup) {
+            value = copy->dup(node->value);
+            if(value == NULL) {
+                listReleaseIter(iter);
+                listRelease(copy);
+                return NULL;
+            }
+        } else {
+            value = node->value;
+        }
+        if((copy = listAddNodeTail(copy, value)) == NULL) {
+            listReleaseIter(iter);
+            listRelease(copy);
+            return NULL;
+        }
+
+    }
+    return copy;
+}
+
+listIter *listGetIterator(list *list, int direction) {
+   listIter *iter = (struct listIter*)zmalloc(sizeof(listIter*));
+   iter->direction = direction;
+   if(direction == FORWARD) {
+       iter->next = list->head;
+   } else {
+       iter->next = list->tail;
+   }
+   return iter;
+}
+
+void listReleaseIter(listIter* iter) {
+   zfree(iter);
+}
+
+listNode *listNext(listIter *iter) {
+    if(!iter->next) return NULL;
+    listNode *node = iter->next;
+    if(iter->direction == FORWARD) {
+        iter->next = (iter->next)->next;
+    } else {
+        iter->next = (iter->next)->prev;
+    }
+    return node;
+}
+
+void listRewind(list *list, listIter *iter) {
+    iter->next = list->head;
+    iter->direction = FORWARD;
+}
+
+void listRewindTail(list *list, listIter *iter) {
+    iter->next = list->tail;
+    iter->direction = BACKWARD;
+}
---
+++ redis/src/adlist.h
@@ -0,0 +1,61 @@
+#ifndef __ADLIST_H
+#define __ADLIST_H
+#include<sys/types.h>
+
+enum DIRCTION {
+    FORWARD = 0,
+    BACKWARD
+};
+
+typedef struct listNode {
+    struct listNode *prev;
+    struct listNode *next;
+    void *value;
+} listNode;
+
+typedef struct listIter {
+    listNode *next;
+    int direction;
+} listIter;
+
+typedef struct list {
+    listNode *head;
+    listNode *tail;
+    size_t len;
+    void *(*dup)(void *ptr);
+    void (*free)(void *ptr);
+    int (*match)(void *ptr, void *key);
+} list;
+
+listNode *listFirst(list *list);
+listNode *listTail(list *list);
+size_t listLength(list *list);
+listNode *listPrevNode(listNode *node);
+listNode *listNextNode(listNode *node);
+void *listNodeValue(listNode *node);
+
+void listSetDupMethod(list *list, void *func);
+void listSetFreeMethod(list *list, void *func);
+void listSetMatchMethod(list *list, void *func);
+
+void *listGetDupMethod(list *list);
+void *listGetFreeMethod(list *list);
+void *listGetMAtchMethod(list *list);
+
+list *listCreate(void);
+void listRelease(list *list);
+void safe_listRelease(list **list);
+list *listAddNodeHead(list *list, void *value);
+list *listAddNodeTail(list *list, void *value);
+void listDelNode(list *list, listNode *node);
+listNode *listSearchKey(list *list, void *key);
+listNode *listIndex(list *list, int index);
+
+// Iterator
+listIter *listGetIterator(list *list, int direction);
+listNode *listNext(listIter *iter);
+void listReleaseIter(listIter *iter);
+void listRewind(list *list, listIter *iter);
+void listRewindTail(list *list, listIter *iter);
+
+#endif
