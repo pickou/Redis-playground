865d59b33c0a283503aeabebc11c5fce37714ffd
junchaoyan <junchaoyan@tencent.com>
2022-07-18 20:30:44 +0800

    encapsulate murmurhash3

--- redis/src/hash.cc
+++ redis/src/hash.cc
@@ -1,7 +1,15 @@
 #include <sys/types.h>
+#include <string.h>
 #include "redis/src/hash.h"
 
 // TODO: use redis siphash && murmurhash
 uint64_t simpleHash(const void *key) {
   return 1;
+}
+
+uint64_t commonHash(const void *key) {
+    uint64_t out[2];
+    const int len = strlen((const char *)key);
+    MurmurHash3_x64_128(key, len, SEED, out);
+    return out[1];
 }
--- redis/src/hash.h
+++ redis/src/hash.h
@@ -2,7 +2,10 @@
 #define __HASH_H_
 #include <sys/types.h>
 #include <stdint.h>
+#include "redis/src/MurmurHash3.h"
 
+static uint32_t SEED = 0x1234ABCD;
 uint64_t simpleHash(const void *key);
+uint64_t commonHash(const void *key);
 
 #endif
